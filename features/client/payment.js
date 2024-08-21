fa6527edd2251af4ca8aa745ed78ed72a6e2fca9
Stanislav <yanut995@gmail.com>
2024-08-21 09:26:06 +0300

    Апгрейдим оплату снова

--- features/client/payment.js
+++ features/client/payment.js
@@ -2,6 +2,20 @@ const texts = require("../../shared/texts");
 const utils = require("../../shared/utils");
 
 module.exports.handle = (bot, chatId) => {
+  // Отправляем сообщение с инструкциями по оплате
   bot.sendMessage(chatId, texts.paymentMessage, { parse_mode: "Markdown" });
-  bot.sendPhoto(chatId, utils.getQrPath(), { caption: "Сканируй и оплати!" });
+
+  // Отправляем QR-код для оплаты
+  bot.sendPhoto(chatId, utils.getQrPath(), {
+    caption: "*Способ 3: Сканируй этот QR-код и плати!*",
+    parse_mode: "Markdown",
+  });
 };
+
+// const texts = require("../../shared/texts");
+// const utils = require("../../shared/utils");
+
+// module.exports.handle = (bot, chatId) => {
+//   bot.sendMessage(chatId, texts.paymentMessage, { parse_mode: "Markdown" });
+//   bot.sendPhoto(chatId, utils.getQrPath(), { caption: "Сканируй и оплати!" });
+// };
