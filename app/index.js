ee9fc614312c8b7fbe7441ac91a0122590f3d9d7
Stanislav <yanut995@gmail.com>
2024-08-21 10:17:23 +0300

    Эксперимент с кнопками

--- app/index.js
+++ app/index.js
@@ -46,30 +46,26 @@ bot.on("callback_query", (callbackQuery) => {
   );
 
   // Обработка нажатий на кнопки тарифов
-  if (action === "buy_vpn") {
-    bot.sendMessage(msg.chat.id, "Выбери нужный тарифный план:", {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: "Тарифы на 1 месяц", callback_data: "monthly_tariffs" }],
-          [
-            {
-              text: "Тарифы на 6 месяцев",
-              callback_data: "semi_annual_tariffs",
-            },
-          ],
-          [{ text: "Оставить комментарий", callback_data: "leave_comment" }],
-        ],
-      },
-    });
-  } else if (action === "monthly_tariffs") {
-    selectTariff.handleTariffSelection(bot, msg.chat.id, "monthly_tariffs");
-  } else if (action === "semi_annual_tariffs") {
-    selectTariff.handleTariffSelection(bot, msg.chat.id, "semi_annual_tariffs");
-  // } else if (action === "payment") {
-  //   bot.sendMessage(msg.chat.id, texts.paymentMessage);
-  // } else if (action === "price") {
-  //   bot.sendMessage(msg.chat.id, texts.priceMessage);
-  }
+  // if (action === "buy_vpn") {
+  //   bot.sendMessage(msg.chat.id, "Выбери нужный тарифный план:", {
+  //     reply_markup: {
+  //       inline_keyboard: [
+  //         [{ text: "Тарифы на 1 месяц", callback_data: "monthly_tariffs" }],
+  //         [
+  //           {
+  //             text: "Тарифы на 6 месяцев",
+  //             callback_data: "semi_annual_tariffs",
+  //           },
+  //         ],
+  //         [{ text: "Оставить комментарий", callback_data: "leave_comment" }],
+  //       ],
+  //     },
+  //   });
+  // } else if (action === "monthly_tariffs") {
+  //   selectTariff.handleTariffSelection(bot, msg.chat.id, "monthly_tariffs");
+  // } else if (action === "semi_annual_tariffs") {
+  //   selectTariff.handleTariffSelection(bot, msg.chat.id, "semi_annual_tariffs");
+  // }
   // Добавляем обработку других нажатий...
 });
 
--- features/client/payment.js
+++ features/client/payment.js
@@ -8,15 +8,7 @@ module.exports.handle = (bot, chatId) => {
   });
 
   bot.sendPhoto(chatId, utils.getQrPath(), {
-    // caption: "*Способ 3: Сканируй этот QR-код и плати!*",
     parse_mode: "Markdown",
   });
 };
 
-// const texts = require("../../shared/texts");
-// const utils = require("../../shared/utils");
-
-// module.exports.handle = (bot, chatId) => {
-//   bot.sendMessage(chatId, texts.paymentMessage, { parse_mode: "Markdown" });
-//   bot.sendPhoto(chatId, utils.getQrPath(), { caption: "Сканируй и оплати!" });
-// };
