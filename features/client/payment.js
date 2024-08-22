9eefc6ae3683f3ba6f78efd83d8afeb870c71b90
Stanislav <yanut995@gmail.com>
2024-08-22 10:37:47 +0300

    Рефакторинг: улучшение оформления страницы оплаты

--- features/client/payment.js
+++ features/client/payment.js
@@ -2,13 +2,26 @@ const texts = require("../../shared/texts");
 const utils = require("../../shared/utils");
 
 module.exports.handle = (bot, chatId) => {
-  bot.sendMessage(chatId, texts.paymentMessage, {
-    parse_mode: "Markdown",
-    disable_web_page_preview: true, // Отключаем предпросмотр ссылок
-  });
-
-  bot.sendPhoto(chatId, utils.getQrPath(), {
-    parse_mode: "Markdown",
-  });
+  bot
+    .sendMessage(chatId, texts.paymentMessage, { parse_mode: "Markdown" })
+    .then(() => {
+      // Переносим QR-код под способ 3
+      bot.sendPhoto(chatId, utils.getQrPath(), {
+        caption: "📷 QR-код для оплаты",
+      });
+    });
 };
 
+// const texts = require("../../shared/texts");
+// const utils = require("../../shared/utils");
+
+// module.exports.handle = (bot, chatId) => {
+//   bot.sendMessage(chatId, texts.paymentMessage, {
+//     parse_mode: "Markdown",
+//     disable_web_page_preview: true, // Отключаем предпросмотр ссылок
+//   });
+
+//   bot.sendPhoto(chatId, utils.getQrPath(), {
+//     parse_mode: "Markdown",
+//   });
+// };
