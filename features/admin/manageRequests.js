b45ef784e856636e4c695d5e55d466c19f990a5b
Stanislav <yanut995@gmail.com>
2024-08-22 11:05:45 +0300

    Правим баги

--- features/admin/manageRequests.js
+++ features/admin/manageRequests.js
@@ -1,25 +1,29 @@
 require("dotenv").config(); // Подключаем dotenv
 const texts = require("../../shared/texts");
 
-module.exports.handle = (bot, msg) => {
+module.exports.handle = (bot, msg, selectedTariff) => {
+  // Добавляем selectedTariff
   const chatId = msg.chat.id;
   const adminChatId = process.env.ADMIN_CHAT_ID; // Используем переменную из .env
   const userName = msg.from.first_name || "Неизвестный пользователь";
-  const tariff = msg.text || "Не указан"; // Здесь нужно как-то получать выбранный тариф. Можно передать его в тексте или сохранить в сессии.
-  const paymentInfo = msg.photo ? "Фото чека" : "Файл с чеком";
-  const comment = msg.caption || ""; // Если пользователь добавил подпись к чеку, используем её как комментарий
+  const paymentInfo = msg.photo
+    ? "Фото чека"
+    : msg.document
+    ? "Файл с чеком"
+    : "Неизвестно";
+  const comment = msg.caption || "Комментарий отсутствует"; // Если пользователь добавил подпись к чеку, используем её как комментарий
 
-  // Отправляем уведомление админу с информацией о заказе
-  const adminNotification = texts.adminNotificationMessage(
-    userName,
-    tariff,
-    paymentInfo,
-    comment
-  );
-  bot.sendMessage(adminChatId, adminNotification, { parse_mode: "Markdown" });
-
-  // Пересылаем чек или документ администратору
+  // Отправляем уведомление админу с информацией о заказе только при получении чека
   if (msg.photo || msg.document) {
+    const adminNotification = texts.adminNotificationMessage(
+      userName,
+      selectedTariff,
+      paymentInfo,
+      comment
+    );
+    bot.sendMessage(adminChatId, adminNotification, { parse_mode: "Markdown" });
+
+    // Пересылаем чек или документ администратору
     bot.forwardMessage(adminChatId, chatId, msg.message_id);
   }
 
--- processes/admin/adminHandler.js
+++ processes/admin/adminHandler.js
@@ -2,8 +2,26 @@ const texts = require("../../shared/texts");
 const manageRequests = require("../../features/admin/manageRequests");
 
 module.exports.init = (bot) => {
-  bot.on("message", (msg) => manageRequests.handle(bot, msg));
+  let selectedTariff = ""; // Добавляем переменную для хранения тарифа
+
+  bot.on("callback_query", (callbackQuery) => {
+    const action = callbackQuery.data;
+    if (action.startsWith("tariff_")) {
+      selectedTariff = action.replace("tariff_", ""); // Сохраняем выбранный тариф
+    }
+  });
+
+  bot.on("message", (msg) => {
+    manageRequests.handle(bot, msg, selectedTariff); // Передаем выбранный тариф в manageRequests
+  });
 };
+
+// const texts = require("../../shared/texts");
+// const manageRequests = require("../../features/admin/manageRequests");
+
+// module.exports.init = (bot) => {
+//   bot.on("message", (msg) => manageRequests.handle(bot, msg));
+// };
 
 // const texts = require("../../shared/texts");
 // const manageRequests = require("../../features/admin/manageRequests");
