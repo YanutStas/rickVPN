31af47a4172babde32b95716bac29c22b9b65c54
Stanislav <yanut995@gmail.com>
2024-08-22 10:01:25 +0300

    Рефакторинг: перемещение логики выбора тарифов в selectTariff.js

--- features/client/selectTariff.js
+++ features/client/selectTariff.js
@@ -1,47 +1,121 @@
 const texts = require("../../shared/texts");
+const payment = require("../../features/client/payment");
+const logger = require("../../app/logger");
 
-module.exports.handle = (bot, chatId, userName) => {
-  const options = {
-    parse_mode: "Markdown",
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: "Тарифы 1 мес.", callback_data: "monthly_tariffs" },
-          { text: "Тарифы 6 мес.", callback_data: "semi_annual_tariffs" },
-        ],
-        [{ text: "Оставить комментарий", callback_data: "leave_comment" }],
-      ],
-    },
-  };
+module.exports.handle = (bot, chatId, action) => {
+  logger.info(`Пользователь выбрал действие: ${action}`);
 
-  bot.sendMessage(chatId, `Выберите нужный тарифный план`, options);
-};
+  switch (action) {
+    case "monthly_tariff":
+      bot.sendMessage(chatId, "Выберите тариф на 1 месяц:", {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "310 рублей/мес", callback_data: "tariff_310_monthly" }],
+            [{ text: "350 рублей/мес", callback_data: "tariff_350_monthly" }],
+            [{ text: "600 рублей/мес", callback_data: "tariff_600_monthly" }],
+          ],
+        },
+      });
+      break;
 
-// Обработчик для выбора тарифов
-module.exports.handleTariffSelection = (bot, chatId, tariffType) => {
-  let message;
-  const options = {
-    parse_mode: "Markdown",
-    reply_markup: {
-      inline_keyboard: [],
-    },
-  };
+    case "semiannual_tariff":
+      bot.sendMessage(chatId, "Выберите тариф на 6 месяцев:", {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "1550 руб./6 мес.",
+                callback_data: "tariff_1550_semiannual",
+              },
+            ],
+            [
+              {
+                text: "1750 руб./6 мес.",
+                callback_data: "tariff_1750_semiannual",
+              },
+            ],
+            [
+              {
+                text: "3000 руб./6 мес.",
+                callback_data: "tariff_3000_semiannual",
+              },
+            ],
+          ],
+        },
+      });
+      break;
 
-  if (tariffType === "monthly_tariffs") {
-    message = texts.tariffSelectionMessage;
-    options.reply_markup.inline_keyboard = [
-      [{ text: "310 рублей/мес.", callback_data: "tariff_310" }],
-      [{ text: "350 рублей/мес.", callback_data: "tariff_350" }],
-      [{ text: "600 рублей/мес.", callback_data: "tariff_600" }],
-    ];
-  } else if (tariffType === "semi_annual_tariffs") {
-    message = texts.semiAnnualTariffMessage;
-    options.reply_markup.inline_keyboard = [
-      [{ text: "1550 руб./6 мес.", callback_data: "tariff_1550" }],
-      [{ text: "1750 руб./6 мес.", callback_data: "tariff_1750" }],
-      [{ text: "3000 руб./6 мес.", callback_data: "tariff_3000" }],
-    ];
+    // Обработка тарифов на 1 месяц
+    case "tariff_310_monthly":
+    case "tariff_350_monthly":
+    case "tariff_600_monthly":
+      bot.sendMessage(chatId, texts.paymentMessage, {
+        parse_mode: "Markdown",
+      });
+      break;
+
+    // Обработка тарифов на 6 месяцев
+    case "tariff_1550_semiannual":
+    case "tariff_1750_semiannual":
+    case "tariff_3000_semiannual":
+      bot.sendMessage(chatId, texts.paymentMessage, {
+        parse_mode: "Markdown",
+      });
+      break;
+
+    default:
+      bot.sendMessage(chatId, "Извините, произошла ошибка. Попробуйте снова.");
+      logger.error(`Неизвестное действие: ${action}`);
+      break;
   }
-
-  bot.sendMessage(chatId, message, options);
 };
+
+
+
+// const texts = require("../../shared/texts");
+
+// module.exports.handle = (bot, chatId, userName) => {
+//   const options = {
+//     parse_mode: "Markdown",
+//     reply_markup: {
+//       inline_keyboard: [
+//         [
+//           { text: "Тарифы 1 мес.", callback_data: "monthly_tariffs" },
+//           { text: "Тарифы 6 мес.", callback_data: "semi_annual_tariffs" },
+//         ],
+//         [{ text: "Оставить комментарий", callback_data: "leave_comment" }],
+//       ],
+//     },
+//   };
+
+//   bot.sendMessage(chatId, `Выберите нужный тарифный план`, options);
+// };
+
+// // Обработчик для выбора тарифов
+// module.exports.handleTariffSelection = (bot, chatId, tariffType) => {
+//   let message;
+//   const options = {
+//     parse_mode: "Markdown",
+//     reply_markup: {
+//       inline_keyboard: [],
+//     },
+//   };
+
+//   if (tariffType === "monthly_tariffs") {
+//     message = texts.tariffSelectionMessage;
+//     options.reply_markup.inline_keyboard = [
+//       [{ text: "310 рублей/мес.", callback_data: "tariff_310" }],
+//       [{ text: "350 рублей/мес.", callback_data: "tariff_350" }],
+//       [{ text: "600 рублей/мес.", callback_data: "tariff_600" }],
+//     ];
+//   } else if (tariffType === "semi_annual_tariffs") {
+//     message = texts.semiAnnualTariffMessage;
+//     options.reply_markup.inline_keyboard = [
+//       [{ text: "1550 руб./6 мес.", callback_data: "tariff_1550" }],
+//       [{ text: "1750 руб./6 мес.", callback_data: "tariff_1750" }],
+//       [{ text: "3000 руб./6 мес.", callback_data: "tariff_3000" }],
+//     ];
+//   }
+
+//   bot.sendMessage(chatId, message, options);
+// };
--- processes/client/clientHandler.js
+++ processes/client/clientHandler.js
@@ -1,8 +1,7 @@
 const texts = require("../../shared/texts");
-const selectTariff = require("../../features/client/selectTariff");
-const payment = require("../../features/client/payment");
 const startPage = require("../../pages/client/start");
-const logger = require("../../app/logger"); // Подключаем логгер
+const selectTariff = require("../../features/client/selectTariff");
+const logger = require("../../app/logger");
 
 module.exports.init = (bot) => {
   bot.onText(/\/start/, (msg) => startPage.render(bot, msg));
@@ -13,7 +12,9 @@ module.exports.init = (bot) => {
     const action = callbackQuery.data;
 
     // Логируем нажатие на кнопку
-    logger.info(`Пользователь ${userName} (${chatId}) нажал на кнопку: ${action}`);
+    logger.info(
+      `Пользователь ${userName} (${chatId}) нажал на кнопку: ${action}`
+    );
 
     switch (action) {
       case "buy_vpn":
@@ -38,80 +39,7 @@ module.exports.init = (bot) => {
         break;
 
       case "choose_tariff":
-        bot.sendMessage(
-          chatId,
-          "Жмякай на кнопки и не тормози(а то вдруг всё закончится?):",
-          {
-            reply_markup: {
-              inline_keyboard: [
-                [{ text: "Тариф на 1 месяц", callback_data: "monthly_tariff" }],
-                [
-                  {
-                    text: "Тариф на 6 месяцев",
-                    callback_data: "semiannual_tariff",
-                  },
-                ],
-              ],
-            },
-          }
-        );
-        break;
-
-      case "monthly_tariff":
-        bot.sendMessage(chatId, "Выберите тариф на 1 месяц:", {
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: "310 рублей/мес", callback_data: "tariff_310_monthly" }],
-              [{ text: "350 рублей/мес", callback_data: "tariff_350_monthly" }],
-              [{ text: "600 рублей/мес", callback_data: "tariff_600_monthly" }],
-            ],
-          },
-        });
-        break;
-
-      case "semiannual_tariff":
-        bot.sendMessage(chatId, "Выберите тариф на 6 месяцев:", {
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "1550 руб./6 мес.",
-                  callback_data: "tariff_1550_semiannual",
-                },
-              ],
-              [
-                {
-                  text: "1750 руб./6 мес.",
-                  callback_data: "tariff_1750_semiannual",
-                },
-              ],
-              [
-                {
-                  text: "3000 руб./6 мес.",
-                  callback_data: "tariff_3000_semiannual",
-                },
-              ],
-            ],
-          },
-        });
-        break;
-
-      // Обработка тарифов на 1 месяц
-      case "tariff_310_monthly":
-      case "tariff_350_monthly":
-      case "tariff_600_monthly":
-        bot.sendMessage(chatId, texts.paymentMessage, {
-          parse_mode: "Markdown",
-        });
-        break;
-
-      // Обработка тарифов на 6 месяцев
-      case "tariff_1550_semiannual":
-      case "tariff_1750_semiannual":
-      case "tariff_3000_semiannual":
-        bot.sendMessage(chatId, texts.paymentMessage, {
-          parse_mode: "Markdown",
-        });
+        selectTariff.handle(bot, chatId, action);
         break;
 
       case "price":
@@ -119,6 +47,7 @@ module.exports.init = (bot) => {
         break;
 
       default:
+        selectTariff.handle(bot, chatId, action); // Обработка выбора тарифа перенесена в selectTariff.js
         break;
     }
   });
@@ -128,51 +57,122 @@ module.exports.init = (bot) => {
 // const selectTariff = require("../../features/client/selectTariff");
 // const payment = require("../../features/client/payment");
 // const startPage = require("../../pages/client/start");
+// const logger = require("../../app/logger"); // Подключаем логгер
 
 // module.exports.init = (bot) => {
 //   bot.onText(/\/start/, (msg) => startPage.render(bot, msg));
 
 //   bot.on("callback_query", (callbackQuery) => {
 //     const chatId = callbackQuery.message.chat.id;
-//     const userName = callbackQuery.from.first_name || "лузер";
+//     const userName = callbackQuery.from.first_name || "друг";
+//     const action = callbackQuery.data;
 
-//     switch (callbackQuery.data) {
+//     // Логируем нажатие на кнопку
+//     logger.info(`Пользователь ${userName} (${chatId}) нажал на кнопку: ${action}`);
+
+//     switch (action) {
 //       case "buy_vpn":
-//         selectTariff.handle(bot, chatId, userName);
+//         bot.sendMessage(
+//           chatId,
+//           "Молодец, а теперь почитай сначала инструкцию, а потом выбери тариф, если всё понятно",
+//           {
+//             reply_markup: {
+//               inline_keyboard: [
+//                 [{ text: "Инструкция", callback_data: "instruction" }],
+//                 [{ text: "Выбор тарифа", callback_data: "choose_tariff" }],
+//               ],
+//             },
+//           }
+//         );
 //         break;
-//       case "view_price":
-//       case "price":
-//         bot.sendMessage(chatId, texts.priceMessage, { parse_mode: "Markdown" });
-//         break;
-//       case "how_to_pay":
-//       case "payment":
-//         payment.handle(bot, chatId);
-//         break;
-//       case "how_to_connect":
-//         bot.sendMessage(chatId, texts.connectMessage, {
+
+//       case "instruction":
+//         bot.sendMessage(chatId, texts.instructionMessage, {
 //           parse_mode: "Markdown",
 //         });
 //         break;
-//       case "tariff_310": // Добавляем обработчики для тарифов
-//       case "tariff_350":
-//       case "tariff_600":
+
+//       case "choose_tariff":
 //         bot.sendMessage(
 //           chatId,
-//           `Вы выбрали тариф: *${callbackQuery.data.replace(
-//             "tariff_",
-//             ""
-//           )} рублей/месяц*. Теперь отправьте чек об оплате.`,
-//           { parse_mode: "Markdown" }
+//           "Жмякай на кнопки и не тормози(а то вдруг всё закончится?):",
+//           {
+//             reply_markup: {
+//               inline_keyboard: [
+//                 [{ text: "Тариф на 1 месяц", callback_data: "monthly_tariff" }],
+//                 [
+//                   {
+//                     text: "Тариф на 6 месяцев",
+//                     callback_data: "semiannual_tariff",
+//                   },
+//                 ],
+//               ],
+//             },
+//           }
 //         );
 //         break;
-//       case "leave_comment":
-//         bot.sendMessage(chatId, "Напишите ваш комментарий:");
-//         bot.once("message", (msg) => {
-//           const comment = msg.text;
-//           bot.sendMessage(chatId, "Спасибо за ваш комментарий!");
-//           // Тут можно сохранить комментарий или обработать его
+
+//       case "monthly_tariff":
+//         bot.sendMessage(chatId, "Выберите тариф на 1 месяц:", {
+//           reply_markup: {
+//             inline_keyboard: [
+//               [{ text: "310 рублей/мес", callback_data: "tariff_310_monthly" }],
+//               [{ text: "350 рублей/мес", callback_data: "tariff_350_monthly" }],
+//               [{ text: "600 рублей/мес", callback_data: "tariff_600_monthly" }],
+//             ],
+//           },
 //         });
 //         break;
+
+//       case "semiannual_tariff":
+//         bot.sendMessage(chatId, "Выберите тариф на 6 месяцев:", {
+//           reply_markup: {
+//             inline_keyboard: [
+//               [
+//                 {
+//                   text: "1550 руб./6 мес.",
+//                   callback_data: "tariff_1550_semiannual",
+//                 },
+//               ],
+//               [
+//                 {
+//                   text: "1750 руб./6 мес.",
+//                   callback_data: "tariff_1750_semiannual",
+//                 },
+//               ],
+//               [
+//                 {
+//                   text: "3000 руб./6 мес.",
+//                   callback_data: "tariff_3000_semiannual",
+//                 },
+//               ],
+//             ],
+//           },
+//         });
+//         break;
+
+//       // Обработка тарифов на 1 месяц
+//       case "tariff_310_monthly":
+//       case "tariff_350_monthly":
+//       case "tariff_600_monthly":
+//         bot.sendMessage(chatId, texts.paymentMessage, {
+//           parse_mode: "Markdown",
+//         });
+//         break;
+
+//       // Обработка тарифов на 6 месяцев
+//       case "tariff_1550_semiannual":
+//       case "tariff_1750_semiannual":
+//       case "tariff_3000_semiannual":
+//         bot.sendMessage(chatId, texts.paymentMessage, {
+//           parse_mode: "Markdown",
+//         });
+//         break;
+
+//       case "price":
+//         bot.sendMessage(chatId, texts.priceMessage, { parse_mode: "Markdown" });
+//         break;
+
 //       default:
 //         break;
 //     }
