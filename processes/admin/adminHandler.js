0c19de580c31548f0736d5d9f5aab5047db9f3b7
Stanislav <yanut995@gmail.com>
2024-08-23 11:30:54 +0300

    Пилим функционал админа

--- processes/admin/adminHandler.js
+++ processes/admin/adminHandler.js
@@ -14,7 +14,7 @@ module.exports.init = (bot) => {
         `Получен чек от пользователя: ${msg.from.first_name} (${chatId})`
       );
 
-      // Просто передаём обработку в manageRequests
+      // Передаём обработку в manageRequests
       manageRequests.handle(bot, msg);
     }
 
@@ -22,21 +22,30 @@ module.exports.init = (bot) => {
     if (msg.text && msg.text.startsWith("key_")) {
       const [_, userId, uniqueKey] = msg.text.split("_");
       const message = `Ваш индивидуальный код для подключения: ${uniqueKey}\n\n${texts.connectMessage}`;
-      bot
-        .sendMessage(userId, message, { parse_mode: "Markdown" })
-        .then(() => {
-          bot.sendMessage(
-            chatId,
-            "Инструкция с ключом отправлена пользователю."
-          );
-        })
-        .catch((error) => {
-          logger.error(`Ошибка при отправке инструкции: ${error.message}`);
-          bot.sendMessage(
-            chatId,
-            "Ошибка при отправке инструкции пользователю."
-          );
-        });
+      if (userId && uniqueKey) {
+        bot
+          .sendMessage(userId, message, { parse_mode: "Markdown" })
+          .then(() => {
+            bot.sendMessage(
+              chatId,
+              "Инструкция с ключом отправлена пользователю."
+            );
+            logger.info(`Ключ ${uniqueKey} отправлен пользователю ${userId}`);
+          })
+          .catch((error) => {
+            logger.error(`Ошибка при отправке инструкции: ${error.message}`);
+            bot.sendMessage(
+              chatId,
+              "Ошибка при отправке инструкции пользователю."
+            );
+          });
+      } else {
+        logger.error("Неправильный формат команды ключа.");
+        bot.sendMessage(
+          chatId,
+          "Неправильный формат команды. Используйте 'key_USERID_ВАШКЛЮЧ'."
+        );
+      }
     }
   });
 
@@ -44,7 +53,6 @@ module.exports.init = (bot) => {
   bot.on("callback_query", (callbackQuery) => {
     const action = callbackQuery.data;
     const adminChatId = callbackQuery.message.chat.id;
-    const messageId = callbackQuery.message.message_id;
 
     if (action.startsWith("confirm_")) {
       const userId = action.replace("confirm_", "");
@@ -58,6 +66,11 @@ module.exports.init = (bot) => {
             adminChatId,
             "Оплата подтверждена. Введите уникальный ключ в формате 'key_USERID_ВАШКЛЮЧ'."
           );
+          logger.info(`Оплата пользователя ${userId} подтверждена.`);
+        })
+        .catch((error) => {
+          logger.error(`Ошибка при подтверждении оплаты: ${error.message}`);
+          bot.sendMessage(adminChatId, "Ошибка при подтверждении оплаты.");
         });
     } else if (action.startsWith("decline_")) {
       const userId = action.replace("decline_", "");
@@ -68,7 +81,14 @@ module.exports.init = (bot) => {
         )
         .then(() => {
           bot.sendMessage(adminChatId, "Оплата не подтверждена.");
+          logger.info(`Оплата пользователя ${userId} не подтверждена.`);
+        })
+        .catch((error) => {
+          logger.error(`Ошибка при отклонении оплаты: ${error.message}`);
+          bot.sendMessage(adminChatId, "Ошибка при отклонении оплаты.");
         });
+    } else {
+      logger.error(`Неизвестное действие: ${action}`);
     }
   });
 };
@@ -92,6 +112,27 @@ module.exports.init = (bot) => {
 //       // Просто передаём обработку в manageRequests
 //       manageRequests.handle(bot, msg);
 //     }
+
+//     // Проверяем, если сообщение от администратора для ввода ключа
+//     if (msg.text && msg.text.startsWith("key_")) {
+//       const [_, userId, uniqueKey] = msg.text.split("_");
+//       const message = `Ваш индивидуальный код для подключения: ${uniqueKey}\n\n${texts.connectMessage}`;
+//       bot
+//         .sendMessage(userId, message, { parse_mode: "Markdown" })
+//         .then(() => {
+//           bot.sendMessage(
+//             chatId,
+//             "Инструкция с ключом отправлена пользователю."
+//           );
+//         })
+//         .catch((error) => {
+//           logger.error(`Ошибка при отправке инструкции: ${error.message}`);
+//           bot.sendMessage(
+//             chatId,
+//             "Ошибка при отправке инструкции пользователю."
+//           );
+//         });
+//     }
 //   });
 
 //   // Обрабатываем нажатия на кнопки от администратора
@@ -110,107 +151,8 @@ module.exports.init = (bot) => {
 //         .then(() => {
 //           bot.sendMessage(
 //             adminChatId,
-//             "Оплата подтверждена. Выберите следующее действие:",
-//             {
-//               reply_markup: {
-//                 inline_keyboard: [
-//                   [
-//                     {
-//                       text: "Отправить инструкцию",
-//                       callback_data: `send_instructions_${userId}`,
-//                     },
-//                     {
-//                       text: "Связаться с поддержкой",
-//                       callback_data: `contact_support_${userId}`,
-//                     },
-//                   ],
-//                 ],
-//               },
-//             }
+//             "Оплата подтверждена. Введите уникальный ключ в формате 'key_USERID_ВАШКЛЮЧ'."
 //           );
-//         });
-//     } else if (action.startsWith("decline_")) {
-//       const userId = action.replace("decline_", "");
-//       bot
-//         .sendMessage(
-//           userId,
-//           "Ваша оплата не подтверждена, подождите в течение 24 часов или напишите в наш чат."
-//         )
-//         .then(() => {
-//           bot.sendMessage(adminChatId, "Оплата не подтверждена.");
-//         });
-//     }
-
-//     // Обработка нажатий на кнопки для отправки инструкций и поддержки
-//     if (action.startsWith("send_instructions_")) {
-//       const userId = action.replace("send_instructions_", "");
-//       const uniqueKey = generateUniqueKey(); // Функция генерации уникального ключа
-
-//       const message = `Ваш индивидуальный код для подключения: ${uniqueKey}\n\n${texts.connectMessage}`;
-//       bot.sendMessage(userId, message, { parse_mode: "Markdown" }).then(() => {
-//         bot.sendMessage(adminChatId, "Инструкция отправлена пользователю.");
-//       });
-//     } else if (action.startsWith("contact_support_")) {
-//       const userId = action.replace("contact_support_", "");
-//       const supportLink = "https://t.me/your_support_chat"; // Ссылка на чат поддержки
-
-//       bot
-//         .sendMessage(
-//           userId,
-//           `Для связи с поддержкой, пожалуйста, перейдите по следующей ссылке: [Чат поддержки](${supportLink})`,
-//           { parse_mode: "Markdown" }
-//         )
-//         .then(() => {
-//           bot.sendMessage(
-//             adminChatId,
-//             "Ссылка на чат поддержки отправлена пользователю."
-//           );
-//         });
-//     }
-//   });
-// };
-
-// // Функция генерации уникального ключа
-// function generateUniqueKey() {
-//   // Здесь можно использовать любой метод генерации ключей
-//   return `ss://${Math.random().toString(36).substr(2, 8)}`;
-// }
-
-// require("dotenv").config();
-// const manageRequests = require("../../features/admin/manageRequests");
-// const logger = require("../../app/logger");
-
-// module.exports.init = (bot) => {
-//   // Обрабатываем сообщения с чеками
-//   bot.on("message", (msg) => {
-//     const chatId = msg.chat.id;
-
-//     // Проверяем, отправил ли пользователь чек (фото или документ)
-//     if (msg.photo || msg.document) {
-//       logger.info(
-//         `Получен чек от пользователя: ${msg.from.first_name} (${chatId})`
-//       );
-
-//       // Просто передаём обработку в manageRequests
-//       manageRequests.handle(bot, msg);
-//     }
-//   });
-
-//   // Обрабатываем нажатия на кнопки от администратора
-//   bot.on("callback_query", (callbackQuery) => {
-//     const action = callbackQuery.data;
-//     const adminChatId = callbackQuery.message.chat.id;
-
-//     // Проверяем действие
-//     if (action.startsWith("confirm_")) {
-//       const userId = action.replace("confirm_", "");
-//       bot
-//         .sendMessage(
-//           userId,
-//           "Ваша оплата подтверждена, ожидайте получение ключа в течение 24 часов."
-//         )
-//         .then(() => {
-//           bot.sendMessage(adminChatId, "Оплата подтверждена.");
 //         });
 //     } else if (action.startsWith("decline_")) {
 //       const userId = action.replace("decline_", "");
