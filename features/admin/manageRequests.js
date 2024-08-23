d40c4f81dde35f50f717c32da9c85d21ae094951
Stanislav <yanut995@gmail.com>
2024-08-23 10:26:03 +0300

    Постепенно изменяем сообщение для админа

--- features/admin/manageRequests.js
+++ features/admin/manageRequests.js
@@ -1,20 +1,32 @@
-require("dotenv").config(); 
+require("dotenv").config();
 const notifications = require("../../widgets/admin/notifications");
 
 module.exports.handle = (bot, msg) => {
-  const adminChatId = process.env.ADMIN_CHAT_ID; 
-  notifications.notifyAdmin(bot, adminChatId, "Бугагашенька");
+  const adminChatId = process.env.ADMIN_CHAT_ID;
+
+  const userName = msg.from.first_name || "Неизвестный пользователь";
+  const userId = msg.from.id;
+  const dateTime = new Date().toLocaleString('ru-RU', {
+    timeZone: 'Europe/Moscow',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  });
+
+  const message = `${dateTime} Пользователь ${userName} (${userId}) оформил заказ`;
+
+  notifications.notifyAdmin(bot, adminChatId, message);
 };
 
 
 
 // require("dotenv").config(); 
+// const notifications = require("../../widgets/admin/notifications");
 
 // module.exports.handle = (bot, msg) => {
 //   const adminChatId = process.env.ADMIN_CHAT_ID; 
-
-//   // Отправляем тестовое сообщение админу
-//   bot.sendMessage(adminChatId, "Бугагашенька").catch((error) => {
-//     console.log(`Ошибка при отправке сообщения админу: ${error.message}`);
-//   });
+//   notifications.notifyAdmin(bot, adminChatId, "Бугагашенька");
 // };
