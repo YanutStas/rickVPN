afa1079555dd8340cceeaa33f6f7aff98217aade
Stanislav <yanut995@gmail.com>
2024-08-23 10:32:20 +0300

    Постепенно изменяем сообщение для админа/Добавили тарифы

--- features/admin/manageRequests.js
+++ features/admin/manageRequests.js
@@ -1,32 +1,70 @@
 require("dotenv").config();
 const notifications = require("../../widgets/admin/notifications");
 
-module.exports.handle = (bot, msg) => {
+module.exports.handle = (bot, msg, tariff) => {
   const adminChatId = process.env.ADMIN_CHAT_ID;
 
   const userName = msg.from.first_name || "Неизвестный пользователь";
   const userId = msg.from.id;
-  const dateTime = new Date().toLocaleString('ru-RU', {
-    timeZone: 'Europe/Moscow',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric'
+  const dateTime = new Date().toLocaleString("ru-RU", {
+    timeZone: "Europe/Moscow",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
   });
 
-  const message = `${dateTime} Пользователь ${userName} (${userId}) оформил заказ`;
+  // Преобразуем тариф в читаемый формат
+  let tariffDescription = "";
+  switch (tariff) {
+    case "tariff_310_monthly":
+      tariffDescription = "на 1 месяц тариф 310 рублей";
+      break;
+    case "tariff_350_monthly":
+      tariffDescription = "на 1 месяц тариф 350 рублей";
+      break;
+    case "tariff_600_monthly":
+      tariffDescription = "на 1 месяц тариф 600 рублей";
+      break;
+    case "tariff_1550_semiannual":
+      tariffDescription = "на 6 месяцев тариф 1550 рублей";
+      break;
+    case "tariff_1750_semiannual":
+      tariffDescription = "на 6 месяцев тариф 1750 рублей";
+      break;
+    case "tariff_3000_semiannual":
+      tariffDescription = "на 6 месяцев тариф 3000 рублей";
+      break;
+    default:
+      tariffDescription = "тариф не выбран";
+  }
+
+  const message = `${dateTime} Пользователь ${userName} (${userId}) оформил заказ ${tariffDescription}`;
 
   notifications.notifyAdmin(bot, adminChatId, message);
 };
 
-
-
-// require("dotenv").config(); 
+// require("dotenv").config();
 // const notifications = require("../../widgets/admin/notifications");
 
 // module.exports.handle = (bot, msg) => {
-//   const adminChatId = process.env.ADMIN_CHAT_ID; 
-//   notifications.notifyAdmin(bot, adminChatId, "Бугагашенька");
+//   const adminChatId = process.env.ADMIN_CHAT_ID;
+
+//   const userName = msg.from.first_name || "Неизвестный пользователь";
+//   const userId = msg.from.id;
+//   const dateTime = new Date().toLocaleString('ru-RU', {
+//     timeZone: 'Europe/Moscow',
+//     hour: '2-digit',
+//     minute: '2-digit',
+//     second: '2-digit',
+//     day: '2-digit',
+//     month: '2-digit',
+//     year: 'numeric'
+//   });
+
+//   const message = `${dateTime} Пользователь ${userName} (${userId}) оформил заказ`;
+
+//   notifications.notifyAdmin(bot, adminChatId, message);
 // };
--- processes/client/clientHandler.js
+++ processes/client/clientHandler.js
@@ -4,8 +4,11 @@ const startPage = require("../../pages/client/start");
 const logger = require("../../app/logger");
 
 module.exports.init = (bot) => {
+  // Хранение выбранного тарифа
+  const userTariffs = {};
+
   bot.onText(/\/start/, (msg) => {
-    startPage.handle(bot, msg); 
+    startPage.handle(bot, msg);
   });
 
   bot.on("callback_query", (callbackQuery) => {
@@ -59,14 +62,54 @@ module.exports.init = (bot) => {
         );
         break;
 
+      // Передаем обработку тарифов в selectTariff.js
       case "monthly_tariff":
+        bot.sendMessage(chatId, "Выберите тариф на 1 месяц:", {
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "310 рублей/мес", callback_data: "tariff_310_monthly" }],
+              [{ text: "350 рублей/мес", callback_data: "tariff_350_monthly" }],
+              [{ text: "600 рублей/мес", callback_data: "tariff_600_monthly" }],
+            ],
+          },
+        });
+        break;
+
       case "semiannual_tariff":
+        bot.sendMessage(chatId, "Выберите тариф на 6 месяцев:", {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "1550 руб./6 мес.",
+                  callback_data: "tariff_1550_semiannual",
+                },
+              ],
+              [
+                {
+                  text: "1750 руб./6 мес.",
+                  callback_data: "tariff_1750_semiannual",
+                },
+              ],
+              [
+                {
+                  text: "3000 руб./6 мес.",
+                  callback_data: "tariff_3000_semiannual",
+                },
+              ],
+            ],
+          },
+        });
+        break;
+
       case "tariff_310_monthly":
       case "tariff_350_monthly":
       case "tariff_600_monthly":
       case "tariff_1550_semiannual":
       case "tariff_1750_semiannual":
       case "tariff_3000_semiannual":
+        // Сохраняем информацию о тарифе для пользователя
+        userTariffs[chatId] = action;
         selectTariff.handle(bot, chatId, action);
         break;
 
@@ -91,9 +134,112 @@ module.exports.init = (bot) => {
       bot.sendMessage(chatId, texts.userCompletionMessage, {
         parse_mode: "Markdown",
       });
+
+      // Передаем тариф для обработки чека
+      manageRequests.handle(bot, msg, userTariffs[chatId]);
       logger.info(
         `Получен чек от пользователя: ${msg.from.first_name} (${chatId})`
       );
     }
   });
 };
+
+// const texts = require("../../shared/texts");
+// const selectTariff = require("../../features/client/selectTariff");
+// const startPage = require("../../pages/client/start");
+// const logger = require("../../app/logger");
+
+// module.exports.init = (bot) => {
+//   bot.onText(/\/start/, (msg) => {
+//     startPage.handle(bot, msg);
+//   });
+
+//   bot.on("callback_query", (callbackQuery) => {
+//     const chatId = callbackQuery.message.chat.id;
+//     const userName = callbackQuery.from.first_name || "друг";
+//     const action = callbackQuery.data;
+
+//     logger.info(
+//       `Пользователь ${userName} (${chatId}) нажал на кнопку: ${action}`
+//     );
+
+//     switch (action) {
+//       case "buy_vpn":
+//         bot.sendMessage(
+//           chatId,
+//           "Молодец, а теперь почитай сначала инструкцию, а потом выбери тариф, если всё понятно.",
+//           {
+//             reply_markup: {
+//               inline_keyboard: [
+//                 [{ text: "Инструкция", callback_data: "instruction" }],
+//                 [{ text: "Выбор тарифа", callback_data: "choose_tariff" }],
+//               ],
+//             },
+//           }
+//         );
+//         break;
+
+//       case "instruction":
+//         bot.sendMessage(chatId, texts.instructionMessage, {
+//           parse_mode: "Markdown",
+//         });
+//         break;
+
+//       case "choose_tariff":
+//         bot.sendMessage(
+//           chatId,
+//           "Жмякай на кнопки и не тормози (а то вдруг всё закончится?):",
+//           {
+//             reply_markup: {
+//               inline_keyboard: [
+//                 [{ text: "Тариф на 1 месяц", callback_data: "monthly_tariff" }],
+//                 [
+//                   {
+//                     text: "Тариф на 6 месяцев",
+//                     callback_data: "semiannual_tariff",
+//                   },
+//                 ],
+//               ],
+//             },
+//           }
+//         );
+//         break;
+
+//       case "monthly_tariff":
+//       case "semiannual_tariff":
+//       case "tariff_310_monthly":
+//       case "tariff_350_monthly":
+//       case "tariff_600_monthly":
+//       case "tariff_1550_semiannual":
+//       case "tariff_1750_semiannual":
+//       case "tariff_3000_semiannual":
+//         selectTariff.handle(bot, chatId, action);
+//         break;
+
+//       case "price":
+//         bot.sendMessage(chatId, texts.priceMessage, { parse_mode: "Markdown" });
+//         break;
+
+//       default:
+//         logger.error(`Неизвестное действие: ${action}`);
+//         bot.sendMessage(
+//           chatId,
+//           "Извините, произошла ошибка. Попробуйте снова."
+//         );
+//         break;
+//     }
+//   });
+
+//   bot.on("message", (msg) => {
+//     const chatId = msg.chat.id;
+
+//     if (msg.photo || msg.document) {
+//       bot.sendMessage(chatId, texts.userCompletionMessage, {
+//         parse_mode: "Markdown",
+//       });
+//       logger.info(
+//         `Получен чек от пользователя: ${msg.from.first_name} (${chatId})`
+//       );
+//     }
+//   });
+// };
